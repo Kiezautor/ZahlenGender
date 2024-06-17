fd696a4e2cbed1117123f4d3f644c862896941c3
Markus_Zemke <markus.zemke@fitline-heidelberg.info>
2024-06-17 20:17:24 +0200

    Initial Commit

---
+++ ZahlenGender/ZahlenGenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZahlenGenderApp.swift
+//  ZahlenGender
+//
+//  Created by Markus Zemke on 17.06.24.
+//
+
+import SwiftUI
+
+@main
+struct ZahlenGenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
